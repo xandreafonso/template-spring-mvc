606ac4b07546e49dd6a3e742fb22dfddf6fb9513
Alexandre Afonso <eu@alexandreafonso.com.br>
2017-02-01 15:06:39 -0200

    Separando configuração de recursos web

--- src/main/java/br/com/alexandreafonso/template/config/SegurancaConfigAdapter.java
+++ src/main/java/br/com/alexandreafonso/template/config/SegurancaConfigAdapter.java
@@ -1,15 +1,20 @@
 package br.com.alexandreafonso.template.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public abstract class SegurancaConfigAdapter extends WebSecurityConfigurerAdapter {
 	
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/webjars/**");
+	}
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/resources/**", "/webjars/**").permitAll()
 				.antMatchers("/desenvolvedor").hasRole("VISUALIZAR_PAG_DESENVOLVEDOR")
 				.antMatchers("/gerente").hasRole("VISUALIZAR_PAG_GERENTE")
 				.anyRequest().authenticated()
